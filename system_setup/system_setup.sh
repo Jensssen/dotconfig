646e35e156f1694605d7bf91495f65826043d4f9
Jensssen <nur2008@shared-files.de>
2021-07-23 13:12:45 +0200

    Added Keepass install

--- system_setup/system_setup.sh
+++ system_setup/system_setup.sh
@@ -35,3 +35,6 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 # Install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install Keepassxc
+sudo apt-get install keepassxc 
