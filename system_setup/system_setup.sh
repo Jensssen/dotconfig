5387ebdfc34364a3c90a32f498b84f2a9f25a762
Jensssen <soeren.erichsen@gmx.de>
2023-04-10 16:38:20 +0200

    Add pyenv install

--- system_setup/system_setup.sh
+++ system_setup/system_setup.sh
@@ -77,6 +77,10 @@ echo "# Enable Starship prompt in Zsh shell" >> ~/.zshrc
 echo 'eval "$(starship init zsh)"' >> ~/.zshrc 
 
 # Install pyenv
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+
 curl https://pyenv.run | zsh
 echo "" ~/.zshrc
 echo "#pyenv" ~/.zshrc
